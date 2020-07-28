bef771c08702d4e43c691f7e57a0ac35e9a30c95
unknown <vamsi3353@gmail.com>
2020-07-28 21:32:38 +0530

    vamsi

--- ddl-pkl.sql
+++ ddl-pkl.sql
@@ -2,32 +2,117 @@
 
 
 -- 1. **Create table city**
+CREATE TABLE CITY
+(
+ID NUMBER(11),
+NAME VARCHAR(50)
+);
 
 -- 2. **Create table referee**
 
+CREATE TABLE REFEREE
+(ID NUMBER(11),
+NAME VARCHAR(50)
+);
 -- 3. **Create table innings**
-
+CREATE TABLE INNINGS
+( ID NUMBER(11),
+INNINGS_NUMBER NUMBER(11)
+);
 -- 4. **Create table extra_type**
+CREATE TABLE EXTRA_TYPE
+( ID NUMBER(11),
+NAME VARCHAR(50)
+);
 
 -- 5. **Create table skill**
+create table SKILL
+( ID NUMBER(11),
+NAME VARCHAR(50)
+);
+
 
 -- 6. **Create table team**
+create table TEAM
+( ID NUMBER(11),
+NAME VARCHAR(50),
+COACH VARCHAR(50),
+HOME_CITY NUMBER(11),
+CAPTAIN NUMBER(11)
+);
 
 -- 7. **Create table player**
+create table PLAYER
+( ID NUMBER(11),
+NAME VARCHAR(50),
+COUNTRY VARCHAR(50),
+SKILL_ID NUMBER(11),
+TEAM_ID NUMBER(11)
+);
 
 -- 8. **Create table venue**
+create table VENUE
+( ID NUMBER(11),
+STADIUM_NAME VARCHAR(50),
+CITY_ID NUMBER(11)
+);
 
 -- 9. **Create table event**
+create table EVENT
+( ID NUMBER(11),
+INNINGS_ID NUMBER(11),
+EVENT_NO NUMBER(11),
+RAIDER_ID NUMBER(11),
+RAID_POINTS NUMBER(11),
+DEFENDING_POINTS NUMBER(11),
+CLOCK_IN_SECONDS NUMBER(11),
+TEAM_ONE_SCORE NUMBER(11),
+TEAM_TWO_SCORE NUMBER(11)
+);
 -- 10. **Create table extra_event**
+create table EXTRA_EVENT
+( ID NUMBER(11),
+EVENT_ID NUMBER(11),
+EVENT_TYPE_ID NUMBER(11),
+POINTS NUMBER(11),
+SCORING_TEAM_ID NUMBER(11)
+);
 
 -- 11. **Create table outcome**
+create table OUTCOME
+(ID NUMBER(11),
+STATUS VARCHAR(100),
+WINNER_TEAM_ID NUMBER(11),
+SCORE NUMBER(11),
+PLAYER_OF_MATCH NUMBER(11)
+);
 
 -- 12. **Create table game**
+create table GAME
+( ID NUMBER(11),
+GAME_DATE DATE,
+TEAM_ID_1 NUMBER(11),
+TEAM_ID_2 NUMBER(11),
+VENUE_ID NUMBER(11),
+OUTCOME_ID NUMBER(11),
+REFEREE_ID_1 NUMBER(11),
+REFEREE_ID_2 NUMBER(11),
+FIRST_INNINGS_ID NUMBER(11),
+SECOND_INNINGS_ID NUMBER(11)
+);
+
 
 -- 13. **Drop table city**
+DROP TABLE CITY;
+
 
 -- 14. **Drop table innings**
+DROP TABLE INNINGS;
 
 -- 15. **Drop table skill**
+DROP TABLE SKILL;
+
+
 
 -- 16. **Drop table extra_type**
+DROP TABLE EXTRA_TYPE;
